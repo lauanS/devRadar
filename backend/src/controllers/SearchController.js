79b6824d33b200a244a1d625451a60de8191c255
lauanS <lauan.s.souza@gmail.com>
2020-01-16 11:30:08 -0300

    Controllers

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,29 @@
+const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
+
+module.exports = {
+  async index(request, response) {
+    // Buscas todos devs num raio de 10km
+    // Filtrar pelas tecnologias
+    const { latitude, longitude, techs } = request.query;
+    const techsArray = parseStringAsArray(techs);
+
+    const devs = await Dev.find({
+      techs : {
+        $in: techsArray,
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: 10000,
+        },
+      },
+    });
+
+    console.log(request.query, techsArray);
+    return response.json({ devs: [] })
+  }
+}
