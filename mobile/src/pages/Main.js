d690ece4d31638f0e6f1102b690c2dfee368e504
lauanS <lauan.s.souza@gmail.com>
2020-01-17 22:59:00 -0300

    barra de pesquisa por devs

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from "react-native";
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from "react-native";
 import MapView, { Marker, Callout } from "react-native-maps";
 import { requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location';
-
+import { MaterialIcons } from '@expo/vector-icons'
 export default function Main( { navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
 
@@ -33,6 +33,7 @@ export default function Main( { navigation }) {
   }
 
   return (
+    <>
     <MapView initialRegion={currentRegion} style={styles.map}>
       <Marker coordinate={{ latitude: -23.5290261, longitude: -47.4769197 }} >
         <Image
@@ -52,6 +53,20 @@ export default function Main( { navigation }) {
         </Callout>
       </Marker>
     </MapView>
+    <View style={styles.searchForm}>
+      <TextInput 
+        style={styles.searchInput} 
+        placeholder="Buscar devs por tech..."
+        placeholderTextColor="#999"
+        autoCapitalize="words"
+        autoCorrect={false}
+      />
+      
+      <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+        <MaterialIcons name="my-location" size={20} color="#FFF" />
+      </TouchableOpacity>
+    </View>
+    </>
   );
 }
 
@@ -84,5 +99,40 @@ const styles = StyleSheet.create({
 
   devTechs: {
       marginTop: 5
+  },
+
+  searchForm: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: 'row',
+  },
+
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#FFF',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    elevation: 2,
+  },
+
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#8E4DFF',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15,
   },
 })
